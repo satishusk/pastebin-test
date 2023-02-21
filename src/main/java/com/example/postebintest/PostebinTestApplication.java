64a3d7c5f237ab6391d258160394bbf698314b98
satishusk <sanpavkaz@gmail.com>
2023-02-22 02:21:47 +0400

    Added the main launch point of the Spring Boot Application

---
+++ src/main/java/com/example/postebintest/PostebinTestApplication.java
@@ -0,0 +1,11 @@
+package com.example.postebintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostebinTestApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PostebinTestApplication.class, args);
+  }
+}
