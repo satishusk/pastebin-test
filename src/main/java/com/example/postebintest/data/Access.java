965c287e5afa0d5e6da36cc71e0f20c88152c1f1
satishusk <sanpavkaz@gmail.com>
2023-02-25 01:22:38 +0400

    Added access json supporting

--- src/main/java/com/example/postebintest/data/Access.java
+++ src/main/java/com/example/postebintest/data/Access.java
@@ -1,5 +1,36 @@
 package com.example.postebintest.data;
 
+import com.example.postebintest.exception.AccessException;
+import com.example.postebintest.exception.TimeException;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Map;
+
 public enum Access {
-  PUBLIC, UNLISTED
+  PUBLIC, UNLISTED;
+
+  private static final Map<String, Access> namesMap = Map.of(
+    "public", PUBLIC,
+    "unlisted", UNLISTED
+  );
+
+  @JsonCreator
+  public static Access forValue(String name) {
+    Access access = namesMap.get(name);
+    if (access != null) {
+      return access;
+    }
+    throw new AccessException("Access type: " + name + " is not supported");
+  }
+
+  @JsonValue
+  public String toValue() {
+    for (Map.Entry<String, Access> entry : namesMap.entrySet()) {
+      if (entry.getValue() == this) {
+        return entry.getKey();
+      }
+    }
+    throw new AccessException("Access type: " + this + " is not supported");
+  }
 }
