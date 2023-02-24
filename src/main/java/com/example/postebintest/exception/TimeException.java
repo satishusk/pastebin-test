7b8dcba6ef42d1d6f319e314e57a441d4e5791c3
satishusk <sanpavkaz@gmail.com>
2023-02-25 00:30:20 +0400

    Added base exception to control timing related errors

---
+++ src/main/java/com/example/postebintest/exception/TimeException.java
@@ -0,0 +1,11 @@
+package com.example.postebintest.exception;
+
+public class TimeException extends RuntimeException {
+  public TimeException() {
+    super();
+  }
+
+  public TimeException(String message) {
+    super(message);
+  }
+}
