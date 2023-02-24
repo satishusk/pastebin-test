d4030e2fd3d13259a68eca091f4b1cb93bd52b89
satishusk <sanpavkaz@gmail.com>
2023-02-25 01:24:25 +0400

    Added expiration logic

--- src/main/java/com/example/postebintest/service/paste/PasteServiceImpl.java
+++ src/main/java/com/example/postebintest/service/paste/PasteServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.postebintest.repository.PasteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.OffsetDateTime;
 import java.util.Optional;
 
 @Service
@@ -20,7 +21,11 @@ public class PasteServiceImpl implements PasteService {
   public Paste getByHashId(String hashId) {
     Optional<Paste> optionalPaste = pasteRepository.findById(hashId);
     if (optionalPaste.isPresent()) {
-      return optionalPaste.get();
+      Paste paste = optionalPaste.get();
+      if (paste.getExpirationEndDateTime().isBefore(OffsetDateTime.now())) {
+        throw new PasteException("Paste with id" + hashId + "not available");
+      }
+      return paste;
     } else {
       throw new PasteException("Paste with id" + hashId + "not found");
     }
