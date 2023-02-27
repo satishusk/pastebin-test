cb83a3d1849d9546ca6c1f44b5b9252cbfe4dd8a
satishusk <sanpavkaz@gmail.com>
2023-02-27 04:20:21 +0400

    Refactoring: removed unused method mapBackward

--- src/main/java/com/example/postebintest/service/mapper/impl/TimeMapper.java
+++ src/main/java/com/example/postebintest/service/mapper/impl/TimeMapper.java
@@ -1,23 +1,18 @@
 package com.example.postebintest.service.mapper.impl;
 
 import com.example.postebintest.data.ExpirationTime;
-import com.example.postebintest.dto.PasteDto;
 import com.example.postebintest.service.mapper.ObjectMapper;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 
 @Component
 public class TimeMapper implements ObjectMapper<ExpirationTime, OffsetDateTime> {
   @Override
   public OffsetDateTime mapForward(ExpirationTime time) {
-    return OffsetDateTime.now().plusMinutes(time.toValue());
-  }
-
-  @Override
-  public ExpirationTime mapBackward(OffsetDateTime offsetDateTime) {
-    Duration duration = Duration.between(OffsetDateTime.now(), offsetDateTime);
-    return ExpirationTime.forValue((int) duration.toMinutes());
+    return time.equals(ExpirationTime.NO_LIMITS)
+      ? OffsetDateTime.of(100000, 1, 1, 0, 0, 0, 0, ZoneOffset.ofHours(0))
+      : OffsetDateTime.now().plusMinutes(time.toValue());
   }
 }
