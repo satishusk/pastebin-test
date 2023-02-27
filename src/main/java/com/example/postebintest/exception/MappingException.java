8236e550d0fdc9a0729448c28b046996b62684c1
satishusk <sanpavkaz@gmail.com>
2023-02-27 19:46:59 +0400

    Added common exception for mapping

---
+++ src/main/java/com/example/postebintest/exception/MappingException.java
@@ -0,0 +1,11 @@
+package com.example.postebintest.exception;
+
+public class MappingException extends RuntimeException {
+  public MappingException() {
+    super();
+  }
+
+  public MappingException(String message) {
+    super(message);
+  }
+}
