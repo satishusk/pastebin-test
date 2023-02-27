bbe599fb14f085fea4b170d55e69ac4638bcd36f
satishusk <sanpavkaz@gmail.com>
2023-02-27 04:23:12 +0400

    Changed repository implementation for controlling expiration time

--- src/main/java/com/example/postebintest/repository/PasteRepository.java
+++ src/main/java/com/example/postebintest/repository/PasteRepository.java
@@ -2,7 +2,18 @@ package com.example.postebintest.repository;
 
 import com.example.postebintest.data.Paste;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface PasteRepository extends JpaRepository<Paste, String> { }
+import java.util.List;
+import java.util.Optional;
+
+public interface PasteRepository {
+
+  Paste save(Paste paste);
+
+  Optional<Paste> getByHashId(String hashId);
+
+  List<Paste> listActualPastes(int count);
+}
